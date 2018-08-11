ba4a7d31188a8442d0cc9e6c18cd192c2b6e46ea
Misael Calvillo Mancilla <calvillomisa@hotmail.com>
2018-08-10 19:42:56 -0500

    React: Add the price to the OrderSummary component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -21,6 +21,7 @@ const orderSummary = (props) => {
       <ul>
         {ingredientsSummary}
       </ul>
+      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
       <p>Continue to Checkout?</p>
       <Button
       btnType="Danger"
