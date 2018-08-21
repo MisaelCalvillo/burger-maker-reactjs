ee84736571d5772d8bf9e8ce8c7a081f070d77bd
Misael Calvillo Mancilla <calvillomisa@hotmail.com>
2018-08-21 01:31:12 -0500

    Pass data between pages

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -98,33 +98,11 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    // this.setState({ loading: true });
-    // const order = {
-    //   ingredients: this.state.ingredients,
-    //   price: this.state.price,
-    //   customer: {
-    //     name: 'Misael Calvillo Mancilla',
-    //     address: {
-    //       street: 'Laguna de Huapango 708',
-    //       zipCode: '50100',
-    //       country: 'Mexico'
-    //     }, 
-    //     email: 'test@test.com', 
-    //   },
-    //   deliveryMethod: 'fastest'
-    // }
-    // // alert('You continue');
-    // axios.post('/orders.json', order)
-    //      .then(response => {
-    //         this.setState({ loading: false, purchasing: false });
-    //      })
-    //      .catch(error => {
-    //         this.setState({ loading: false, purchasing: false });
-    //      });
     const queryParams = [];
     for (let i in this.state.ingredients) {
       queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
     }
+    queryParams.push('price=' + this.state.totalPrice);
     const queryString = queryParams.join('&');
     this.props.history.push({
       pathname: '/checkout',
