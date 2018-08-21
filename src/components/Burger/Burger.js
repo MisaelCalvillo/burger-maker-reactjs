4044fa3c92936e5a98c501de2205bf77cd46eb0c
Misael Calvillo Mancilla <calvillomisa@hotmail.com>
2018-08-20 23:08:18 -0500

    History: Change a route witch history.push method

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,10 +1,11 @@
 import React from 'react';
+// import {withRouter} from 'react-router-dom';
 
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-
+  console.log(props)
   let transformedIngredients = Object.keys(props.ingredients)
       .map(igKey => {
         return [...Array(props.ingredients[igKey])].map((_, i) => {
@@ -27,5 +28,6 @@ const burger = (props) => {
     </div>
   );
 }
-
+/* Use HOC withRouter */
+// export default withRouter(burger);
 export default burger;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -28,6 +28,7 @@ class BurgerBuilder extends Component {
   }
 
   componentDidMount () {
+    console.log(this.props)
     axios.get('https://react-my-burger-misa.firebaseio.com/ingredients.json')
          .then(response => {
             this.setState({ ingredients: response.data });
@@ -97,29 +98,30 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    this.setState({ loading: true });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.price,
-      customer: {
-        name: 'Misael Calvillo Mancilla',
-        address: {
-          street: 'Laguna de Huapango 708',
-          zipCode: '50100',
-          country: 'Mexico'
-        }, 
-        email: 'test@test.com', 
-      },
-      deliveryMethod: 'fastest'
-    }
-    // alert('You continue');
-    axios.post('/orders.json', order)
-         .then(response => {
-            this.setState({ loading: false, purchasing: false });
-         })
-         .catch(error => {
-            this.setState({ loading: false, purchasing: false });
-         });
+    // this.setState({ loading: true });
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.price,
+    //   customer: {
+    //     name: 'Misael Calvillo Mancilla',
+    //     address: {
+    //       street: 'Laguna de Huapango 708',
+    //       zipCode: '50100',
+    //       country: 'Mexico'
+    //     }, 
+    //     email: 'test@test.com', 
+    //   },
+    //   deliveryMethod: 'fastest'
+    // }
+    // // alert('You continue');
+    // axios.post('/orders.json', order)
+    //      .then(response => {
+    //         this.setState({ loading: false, purchasing: false });
+    //      })
+    //      .catch(error => {
+    //         this.setState({ loading: false, purchasing: false });
+    //      });
+    this.props.history.push('/checkout');
   }
 
   render() {
