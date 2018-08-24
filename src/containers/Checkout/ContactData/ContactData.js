e55717c5dccfab6cf9b5b706a584f918cee43fc7
Misael Calvillo Mancilla <calvillomisa@hotmail.com>
2018-08-24 01:36:59 -0500

    Forms: Add some simple validation to the form inputs

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -16,7 +16,11 @@ class ContactData extends Component {
             type: 'text',
             placeholder: 'Your name'
           },
-          value: ''
+          value: '',
+          validation: {
+            required: true
+          },
+          valid: false
         },
         street: {
           elementType: 'input',
@@ -24,7 +28,11 @@ class ContactData extends Component {
             type: 'text',
             placeholder: 'Street'
           },
-          value: ''
+          value: '',
+          validation: {
+            required: true
+          },
+          valid: false
         },
         zipCode: {
           elementType: 'input',
@@ -32,7 +40,13 @@ class ContactData extends Component {
             type: 'text',
             placeholder: 'ZIP Code'
           },
-          value: ''
+          value: '',
+          validation: {
+            required: true,
+            minLength: 5,
+            maxLength: 5,
+          },
+          valid: false
         },
         country: {
           elementType: 'input',
@@ -40,7 +54,11 @@ class ContactData extends Component {
             type: 'text',
             placeholder: 'Country'
           },
-          value: ''
+          value: '',
+          validation: {
+            required: true
+          },
+          valid: false
         },
         email: {
           elementType: 'input',
@@ -48,7 +66,11 @@ class ContactData extends Component {
             type: 'email',
             placeholder: 'Your E-Mail'
           },
-          value: ''
+          value: '',
+          validation: {
+            required: true
+          },
+          valid: false
         }, 
         deliveryMethod:{
           elementType: 'select',
@@ -58,7 +80,7 @@ class ContactData extends Component {
               { value: 'cheapest', displayValue: 'Cheapest'},
             ]
           },
-          value: ''
+          value: '',
         },
       },
       
@@ -88,6 +110,22 @@ class ContactData extends Component {
            });
   }
 
+  checkValidity(value, rules) {
+    let isValid = false;
+
+      if (rules.required) {
+          isValid = value.trim() !== '';
+      }
+      if (rules.minLength) {
+        isValid = value.length >= rules.minLength;
+      }
+      if (rules.maxLength) {
+        isValid = value.length <= rules.minLength;
+      }
+
+      return isValid;
+  }
+
   inputChangedHandler = (event, inputIdentifier) => {
       const updatedOrderForm = {
         ...this.state.orderForm
@@ -95,7 +133,8 @@ class ContactData extends Component {
       const updatedFormElement = {...updatedOrderForm[inputIdentifier] }
 
       updatedFormElement.value = event.target.value;
-
+      updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation )
+      console.log(updatedFormElement)
       updatedOrderForm[inputIdentifier] = updatedFormElement;
       this.setState({ orderForm: updatedOrderForm })
   }
