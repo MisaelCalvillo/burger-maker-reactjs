d825d0737279233461ac64e6b58b6f51f2ab658f
Misael Calvillo Mancilla <calvillomisa@hotmail.com>
2018-08-24 00:19:11 -0500

    Forms: Add an Input component with type as a select

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -16,9 +16,19 @@ const input = (props) => {
 
     case ('textarea'): 
       inputElement = <textarea 
-          classsclassName={classes.Label}   
+          className={classes.InputElement}   
           {...props.elementConfig} 
           value={props.value}/>;
+      break;
+    case ('select'): 
+      inputElement = (
+      <select
+          className={classes.InputElement}   
+          value={props.value}>
+          {props.elementConfig.options.map(option => (
+            <option key={option.value} value={option.value}>{option.displayValue}</option>
+          ))}
+      </select>);
       break;
 
     default: 
